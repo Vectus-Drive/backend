31444c74ecf90517522370ba5aa2637346ac7150
Amila619 <amilasandeepa349@gmail.com>
2025-11-01 19:15:43 +0530

    - got a character issue while sending otps switched to utf-8

--- api/utils/smtp_server.py
+++ api/utils/smtp_server.py
@@ -17,13 +17,16 @@ class OTP:
     def generate_otp(self):
         self.otp = random.randint(111111, 999999)
         return self.otp
+    
+    def clean_str(self, s):
+        return s.encode('utf-8', 'ignore').decode('utf-8')
 
     def send_otp(self, to_email):
         msg = EmailMessage()
-        msg["Subject"] = "OTP Verification"
-        msg["From"] = self.GMAIL_USERNAME
-        msg["To"] = to_email
-        msg.set_content(f"Your OTP Code: {self.otp}")
+        msg['Subject'] = self.clean_str("OTP Verification")
+        msg['From'] = self.clean_str(self.GMAIL_USERNAME)
+        msg['To'] = self.clean_str(to_email)
+        msg.set_content(f"Your OTP Code: {self.otp}", charset="utf-8")
 
         with smtplib.SMTP(self.SMTP_SERVER, self.SMTP_PORT) as server:
             server.ehlo()
